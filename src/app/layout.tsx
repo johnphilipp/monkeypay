55ac27fc2b0e6cc669e87da0a377969b4e4cdfe5
Philipp John <philipp.john98@gmail.com>
2025-07-10 20:40:16 +0200

    add ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <GoogleAnalytics gaId="G-J07MV9LM06" />
       </body>
     </html>
   );
